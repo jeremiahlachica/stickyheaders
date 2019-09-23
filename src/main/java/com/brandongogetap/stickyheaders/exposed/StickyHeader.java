8d1c2e589b37e149f08bbc83e4d7dc6b1594670c
Jeremiah Lachica <jeremiahlachica@outlook.com>
2019-09-23 13:18:46 +0800

    Delete all

--- src/main/java/com/brandongogetap/stickyheaders/exposed/StickyHeader.java
+++
@@ -1,9 +0,0 @@
-package com.brandongogetap.stickyheaders.exposed;
-
-/**
- * Marker interface used in {@link com.brandongogetap.stickyheaders.StickyHeaderPositioner()}
- *
- * This should be set on any RecyclerView data item that should act as a Sticky Header
- */
-public interface StickyHeader {
-}
